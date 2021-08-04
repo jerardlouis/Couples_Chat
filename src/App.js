9bfc0f59ba948ae3294183ee642a92a486903479
Jerard Dayanghirang Guevarra <jerardlguevarra@gmail.com>
2021-08-04 17:25:17 -0400

    Added fix

--- src/App.js
+++ src/App.js
@@ -5,18 +5,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h3>My Dear Sweet Ysabela,</h3>
+        <p>Hey baby, I love you lots</p>
+        
+        <p>I hope you're having a good day at owrk today baby</p>
+
+        <p>So far I've been having issues setting up but later I'll build the message server and database</p>
+        
+        <p>For now here's a cute gif lol</p>
+        <img src="https://media.tenor.com/images/9c36df84de37aedbeb8782a3f1c7d8bd/tenor.gif"/>
       </header>
     </div>
   );
